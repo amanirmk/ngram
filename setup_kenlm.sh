a78ae12cafd43207cd8e6f5befefaa80166cb80c
amanirmk <amainakilaas@hmc.edu>
2024-03-20 19:53:47 -0400

    setup appears to work, only for my device likely though

--- setup_kenlm.sh
+++ setup_kenlm.sh
@@ -1,16 +1,26 @@
-set -e
+if $(conda info --envs | grep '*' | grep -q 'base'); then
+  set -e
 
-export CXX=clang++
-export CC=clang
-export CXXFLAGS+=" -std=c++11"
-export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+  export CXX=clang++
+  export CC=clang
+  export CXXFLAGS+=" -std=c++11"
+  export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
 
-brew install cmake boost eigen
-git clone https://github.com/kpu/kenlm.git
-cd kenlm
+  if [ -d "kenlm" ]; then
+    echo "kenlm is already downloaded"
+  else
+    brew install cmake boost eigen
+    git clone https://github.com/kpu/kenlm.git
+  fi
 
-mkdir build && cd build
-cmake ..
-make -j2
-
-# try getting kenlm from surprisal
+  if [ -d "kenlm" ] && [ -d "kenlm/build" ] && [ -d "kenlm/build/bin" ] && $(ls -A "kenlm/build/bin" | grep -q .); then
+    echo "kenlm is already built"
+  else
+    cd kenlm
+    mkdir build && cd build
+    cmake ..
+    make -j2
+  fi
+else
+  echo "Please deactivate your environment and return to base before running this script."
+fi
